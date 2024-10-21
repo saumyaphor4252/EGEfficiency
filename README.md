a2f1c9e2d895a40fac0774acf20435c18293a164
Saumya Phor <saumya.phor4252@gmail.com>
2024-10-21 10:55:48 +0200

    Update the python cfg file

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ scram b -j 10
 cd EGTools/TrigTools/test
 
 ### Reference
+# /RelValZEE_14/CMSSW_14_0_9-PU_140X_mcRun3_2024_realistic_v14_RV245_2024-v1/GEN-SIM-DIGI-RAW
 hltGetConfiguration /dev/CMSSW_14_0_0/GRun/V156 --path HLTriggerFirstPath,HLT_Ele30_WPTight_Gsf_v7,HLT_Ele32_WPTight_Gsf_v21,HLT_Ele115_CaloIdVT_GsfTrkIdT_v21,HLT_Ele135_CaloIdVT_GsfTrkIdT_v14,HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v25,HLT_DoubleEle33_CaloIdL_MW_v24,HLTriggerFinalPath --output minimal --mc --process HLTX --type GRun --globaltag 140X_mcRun3_2024_realistic_v14 --max-events -1 --unprescale --eras Run3 --l1-emulator FullMC --l1 L1Menu_Collisions2024_v1_2_0_xml --input dataset:/RelValZEE_14/CMSSW_14_0_9-PU_140X_mcRun3_2024_realistic_v14_RV245_2024-v1/GEN-SIM-DIGI-RAW --customise HLTrigger/Configuration/customizeHLTforEGamma.customiseEGammaMenuDev,HLTrigger/Configuration/customizeHLTforEGamma.customiseEGammaInputContent > hlt.py
 vi hlt.py # Check maxEvents, output.root file and input files fileNames
 edmConfigDump hlt.py > hlt_config_Reference.py
@@ -23,17 +24,25 @@ edmConfigDump hlt.py > hlt_config_Reference.py
 ./sub_total.jobb
 
 ### Scenario 1
+# /RelValZEE_14/CMSSW_14_0_9-PU_140X_mcRun3_2024_realistic_EOR3_TkDPGv2_RV245_2024-v4/GEN-SIM-DIGI-RAW
 hltGetConfiguration /dev/CMSSW_14_0_0/GRun/V156 --path HLTriggerFirstPath,HLT_Ele30_WPTight_Gsf_v7,HLT_Ele32_WPTight_Gsf_v21,HLT_Ele115_CaloIdVT_GsfTrkIdT_v21,HLT_Ele135_CaloIdVT_GsfTrkIdT_v14,HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v25,HLT_DoubleEle33_CaloIdL_MW_v24,HLTriggerFinalPath --output minimal --mc --process HLTX --type GRun --globaltag 140X_mcRun3_2024_realistic_EOR3_TkDPGv2 --max-events -1 --unprescale --eras Run3 --l1-emulator FullMC --l1 L1Menu_Collisions2024_v1_2_0_xml --input dataset:/RelValZEE_14/CMSSW_14_0_9-PU_140X_mcRun3_2024_realistic_EOR3_TkDPGv2_RV245_2024-v4/GEN-SIM-DIGI-RAW --customise HLTrigger/Configuration/customizeHLTforEGamma.customiseEGammaMenuDev,HLTrigger/Configuration/customizeHLTforEGamma.customiseEGammaInputContent > hlt1.py
 edmConfigDump hlt.py > hlt_config_EOR3_v1.py
 ./cmsCondorData.py hlt_config_EOR3_v1.py /afs/cern.ch/work/s/ssaumya/private/Egamma/TrackerStudies/CMSSW_14_0_9/src/ /eos/user/s/ssaumya/EGammaHLT/Run3Winter/TrackerStudies/EOR31 -n 15 -q workday -p /afs/cern.ch/user/s/ssaumya/private/x509up_u122184
 ./sub_total.jobb
 
 ### Scenario 2
+# /RelValZEE_14/CMSSW_14_0_9-PU_140X_mcRun3_2024_realistic_EOR3_TkDPGv6_RV245_2024-v1/GEN-SIM-DIGI-RAW
 hltGetConfiguration /dev/CMSSW_14_0_0/GRun/V156 --path HLTriggerFirstPath,HLT_Ele30_WPTight_Gsf_v7,HLT_Ele32_WPTight_Gsf_v21,HLT_Ele115_CaloIdVT_GsfTrkIdT_v21,HLT_Ele135_CaloIdVT_GsfTrkIdT_v14,HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v25,HLT_DoubleEle33_CaloIdL_MW_v24,HLTriggerFinalPath --output minimal --mc --process HLTX --type GRun --globaltag 140X_mcRun3_2024_realistic_EOR3_TkDPGv6 --max-events -1 --unprescale --eras Run3 --l1-emulator FullMC --l1 L1Menu_Collisions2024_v1_2_0_xml --input dataset:/RelValZEE_14/CMSSW_14_0_9-PU_140X_mcRun3_2024_realistic_EOR3_TkDPGv6_RV245_2024-v1/GEN-SIM-DIGI-RAW --customise HLTrigger/Configuration/customizeHLTforEGamma.customiseEGammaMenuDev,HLTrigger/Configuration/customizeHLTforEGamma.customiseEGammaInputContent > hlt2.py
 edmConfigDump hlt.py > hlt_config_EOR3_v2.py
 ./cmsCondorData.py hlt_config_EOR3_v2.py /afs/cern.ch/work/s/ssaumya/private/Egamma/TrackerStudies/CMSSW_14_0_9/src/ /eos/user/s/ssaumya/EGammaHLT/Run3Winter/TrackerStudies/EOR32 -n 15 -q workday -p /afs/cern.ch/user/s/ssaumya/private/x509up_u122184
 ./sub_total.jobb
 
-### For the desired path
-cmsRun EfficiencyCalculator
+# For the desired path, update the path in plugins/EfficiencyCalculator.cc
+
+# Update the GT, dirName for hlt.root files and the outputfile name in test/runEfficiency_Calculator.py
+# process.GlobalTag = GlobalTag(process.GlobalTag, '140X_mcRun3_2024_realistic_EOR3_TkDPGv2')
+# dir1Name = "/eos/user/s/ssaumya/EGammaHLT/Run3Winter/TrackerStudies/EOR3_v1/"
+# fileName = cms.string("Efficiency_Calculator_2024_Tracker_EOR3_Studies_Reference.root")
+
+cmsRun EfficiencyCalculator.py
 ```
--- TrigTools/test/run_EfficiencyCalculator.py
+++ TrigTools/test/run_EfficiencyCalculator.py
@@ -9,236 +9,42 @@ reRunL1 = False
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load("FWCore.MessageService.MessageLogger_cfi")
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '131X_mcRun3_2023_realistic_forEGamma_v1') 
+
+#process.GlobalTag = GlobalTag(process.GlobalTag, '140X_mcRun3_2024_realistic_v14') 
+process.GlobalTag = GlobalTag(process.GlobalTag, '140X_mcRun3_2024_realistic_EOR3_TkDPGv2')
+#process.GlobalTag = GlobalTag(process.GlobalTag, '140X_mcRun3_2024_realistic_EOR3_TkDPGv6')
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
+# Input Files
+#dir1Name = "/eos/user/s/ssaumya/EGammaHLT/Run3Winter/TrackerStudies/Reference/"
+dir1Name = "/eos/user/s/ssaumya/EGammaHLT/Run3Winter/TrackerStudies/EOR3_v1/"
+#dir1Name = "/eos/user/s/ssaumya/EGammaHLT/Run3Winter/TrackerStudies/EOR3_v2"
 
-# dir1Name = "/afs/cern.ch/user/r/rselvati/work/private/testBPIX/CMSSW_13_0_10/src/SteamRatesEdmWorkflow/Prod/hltModified/"
-# dir2Name = "/eos/cms/store/mc/Run3Summer22EEDR/ZprimeToEE_M-6000_TuneCP5_13p6TeV-pythia8/GEN-SIM-RAW/Poisson70KeepRAW_124X_mcRun3_2022_realistic_postEE_v1-v1/720000/"
-# fileList1 = filter(os.path.isfile, glob.glob(dir1Name + "*.root"))
-# fileList2 = filter(os.path.isfile, glob.glob(dir2Name + "*.root"))
-# fList = []
-# for f in fileList1:
-#      fs = str(f).replace("/afs/","file:/afs/")
-#      fList.append(fs)
-# for f in fileList2:
-#     fs = str(f).replace("/eos/","file:/eos/")
-#     fList.append(fs)
-# print(fList)
-#from list_relval import fileList
+fileList1 = filter(os.path.isfile, glob.glob(dir1Name + "*.root"))
+fList = []
+for f in fileList1:
+    fs = str(f).replace("/eos/","file:/eos/")
+    fList.append(fs)
+print(fList)
 process.source = cms.Source("PoolSource",
-                                fileNames = cms.untracked.vstring(#fList
-                                    'root://cms-xrd-global.cern.ch//store/data/Run2016B/SingleElectron/MINIAOD/17Jul2018_ver2-v1/80000/FEAB7D8A-048C-E811-A513-AC1F6B1AEFFC.root'
-                )
-                            )
-process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
-#Output file
-process.TFileService = cms.Service("TFileService",
-   fileName = cms.string("Efficiency_Modified.root")
+    fileNames = cms.untracked.vstring(fList
+    #'root://cms-xrd-global.cern.ch//store/data/Run2016B/SingleElectron/MINIAOD/17Jul2018_ver2-v1/80000/FEAB7D8A-048C-E811-A513-AC1F6B1AEFFC.root'
+    )
 )
 
+process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 
-# process.hltEgammaCandidates = cms.EDProducer( "EgammaHLTRecoEcalCandidateProducers",
-#     scHybridBarrelProducer = cms.InputTag( 'hltParticleFlowSuperClusterECALL1Seeded','hltParticleFlowSuperClusterECALBarrel' ),
-#     scIslandEndcapProducer = cms.InputTag( 'hltParticleFlowSuperClusterECALL1Seeded','hltParticleFlowSuperClusterECALEndcapWithPreshower' ),
-#     recoEcalCandidateCollection = cms.string( "" )
-# )
-
-# process.GlobalParametersRcdSource = cms.ESSource("EmptyESSource",
-#     recordName = cms.string('L1TGlobalParametersRcd'),
-#     iovIsRunNotTime = cms.bool(True),
-#     firstValid = cms.vuint32(1)
-# )
-
-# process.GlobalParameters = cms.ESProducer("StableParametersTrivialProducer", 
-#     # bx in event
-#     #NumberBxInEvent = cms.int32(5),
-                                    
-#     # trigger decision
-                                            
-#     # number of physics trigger algorithms
-#     NumberPhysTriggers = cms.uint32(512),
-                                              
-                                              
-#     # trigger objects
-
-#     # muons
-#     NumberL1Muon = cms.uint32(8),
-    
-#     # e/gamma and isolated e/gamma objects
-#     NumberL1EGamma = cms.uint32(12),
-
-#     #  jets
-#     NumberL1Jet = cms.uint32(12),
-
-#     # taus
-#     NumberL1Tau = cms.uint32(12),
-
-#     # hardware
-                                            
-#     # number of maximum chips defined in the xml file
-#     NumberChips = cms.uint32(1),
-
-#     # number of pins on the GTL condition chips
-#     PinsOnChip = cms.uint32(512),
-
-#     # correspondence "condition chip - GTL algorithm word" in the hardware
-#     # e.g.: chip 2: 0 - 95;  chip 1: 96 - 128 (191)
-#     OrderOfChip = cms.vint32(1),
-# )
-
-# process.hltGtStage2Digis = cms.EDProducer( "L1TRawToDigi",
-#     CTP7 = cms.untracked.bool( False ),
-#     DmxFWId = cms.uint32( 0 ),
-#     FWId = cms.uint32( 0 ),
-#     FWOverride = cms.bool( False ),
-#     FedIds = cms.vint32( [ 1404 ] ),
-#     InputLabel = cms.InputTag( "rawDataCollector" ),
-#     MTF7 = cms.untracked.bool( False ),
-#     MinFeds = cms.uint32( 0 ),
-#     Setup = cms.string( "stage2::GTSetup" ),
-#     TMTCheck = cms.bool( True ),
-#     debug = cms.untracked.bool( False ),
-#     lenAMC13Header = cms.untracked.int32( 8 ),
-#     lenAMC13Trailer = cms.untracked.int32( 8 ),
-#     lenAMCHeader = cms.untracked.int32( 8 ),
-#     lenAMCTrailer = cms.untracked.int32( 0 ),
-#     lenSlinkHeader = cms.untracked.int32( 8 ),
-#     lenSlinkTrailer = cms.untracked.int32( 8 )
-# )
-
-# process.hltGtStage2ObjectMap = cms.EDProducer( "L1TGlobalProducer",
-#     AlgoBlkInputTag = cms.InputTag( "hltGtStage2Digis" ),
-#     AlgorithmTriggersUnmasked = cms.bool( True ),
-#     AlgorithmTriggersUnprescaled = cms.bool( True ),
-#     AlternativeNrBxBoardDaq = cms.uint32( 0 ),
-#     BstLengthBytes = cms.int32( -1 ),
-#     EGammaInputTag = cms.InputTag( "hltGtStage2Digis","EGamma" ),
-#     EmulateBxInEvent = cms.int32( 1 ),
-#     EtSumInputTag = cms.InputTag( "hltGtStage2Digis","EtSum" ),
-#     ExtInputTag = cms.InputTag( "hltGtStage2Digis" ),
-#     GetPrescaleColumnFromData = cms.bool( False ),
-#     JetInputTag = cms.InputTag( "hltGtStage2Digis","Jet" ),
-#     L1DataBxInEvent = cms.int32( 5 ),
-#     MuonInputTag = cms.InputTag( "hltGtStage2Digis","Muon" ),
-#     MuonShowerInputTag = cms.InputTag( "hltGtStage2Digis","MuonShower" ),
-#     PrescaleSet = cms.uint32( 1 ),
-#     PrintL1Menu = cms.untracked.bool( False ),
-#     ProduceL1GtDaqRecord = cms.bool( True ),
-#     ProduceL1GtObjectMapRecord = cms.bool( True ),
-#     RequireMenuToMatchAlgoBlkInput = cms.bool( True ),
-#     TauInputTag = cms.InputTag( "hltGtStage2Digis","Tau" ),
-#     TriggerMenuLuminosity = cms.string( "startup" ),
-#     Verbosity = cms.untracked.int32( 0 ),
-#     resetPSCountersEachLumiSec = cms.bool( True ),
-#     semiRandomInitialPSCounters = cms.bool( False ),
-#     useMuonShowers = cms.bool( True )
-# )
-
-
-# process.hltL1sSingleEGor = cms.EDFilter( "HLTL1TSeed",
-#     saveTags = cms.bool( True ),
-#     L1SeedsLogicalExpression = cms.string( "L1_SingleEG38er2p5 OR L1_SingleEG40er2p5 OR L1_SingleEG42er2p5 OR L1_SingleEG45er2p5 OR L1_SingleEG60 OR L1_SingleEG34er2p5 OR L1_SingleEG36er2p5 OR L1_SingleIsoEG30er2p1 OR L1_SingleIsoEG32er2p1 OR L1_SingleIsoEG30er2p5 OR L1_SingleIsoEG32er2p5 OR L1_SingleIsoEG34er2p5"), #opt4
-#     L1ObjectMapInputTag = cms.InputTag( "hltGtStage2ObjectMap" ),
-#     L1GlobalInputTag = cms.InputTag( "hltGtStage2Digis" ),
-#     L1MuonInputTag = cms.InputTag( 'hltGtStage2Digis','Muon' ),
-#     L1MuonShowerInputTag = cms.InputTag( 'hltGtStage2Digis','MuonShower' ),
-#     L1EGammaInputTag = cms.InputTag( 'hltGtStage2Digis','EGamma' ),
-#     L1JetInputTag = cms.InputTag( 'hltGtStage2Digis','Jet' ),
-#     L1TauInputTag = cms.InputTag( 'hltGtStage2Digis','Tau' ),
-#     L1EtSumInputTag = cms.InputTag( 'hltGtStage2Digis','EtSum' )
-# )
-
-# process.hltL1sSingleAndDoubleEG = cms.EDFilter( "HLTL1TSeed",
-#     saveTags = cms.bool( True ),
-#     L1SeedsLogicalExpression = cms.string( "L1_SingleEG26er2p5 OR L1_SingleEG38er2p5 OR L1_SingleEG40er2p5 OR L1_SingleEG42er2p5 OR L1_SingleEG45er2p5 OR L1_SingleEG60 OR L1_SingleEG34er2p5 OR L1_SingleEG36er2p5 OR L1_SingleIsoEG30er2p1 OR L1_SingleIsoEG32er2p1 OR L1_SingleIsoEG30er2p5 OR L1_SingleIsoEG32er2p5 OR L1_SingleIsoEG34er2p5 OR L1_DoubleEG_22_10_er2p5 OR L1_DoubleEG_25_12_er2p5 OR L1_DoubleEG_25_14_er2p5 OR L1_DoubleEG_LooseIso22_12_er2p5" ),
-#     L1ObjectMapInputTag = cms.InputTag( "hltGtStage2ObjectMap" ),
-#     L1GlobalInputTag = cms.InputTag( "hltGtStage2Digis" ),
-#     L1MuonInputTag = cms.InputTag( 'hltGtStage2Digis','Muon' ),
-#     L1MuonShowerInputTag = cms.InputTag( 'hltGtStage2Digis','MuonShower' ),
-#     L1EGammaInputTag = cms.InputTag( 'hltGtStage2Digis','EGamma' ),
-#     L1JetInputTag = cms.InputTag( 'hltGtStage2Digis','Jet' ),
-#     L1TauInputTag = cms.InputTag( 'hltGtStage2Digis','Tau' ),
-#     L1EtSumInputTag = cms.InputTag( 'hltGtStage2Digis','EtSum' )
-# )
-
-# process.hltL1sSingleEG40to50 = cms.EDFilter( "HLTL1TSeed",
-#     saveTags = cms.bool( True ),
-#     L1SeedsLogicalExpression = cms.string( "L1_SingleEG40er2p5 OR L1_SingleEG42er2p5 OR L1_SingleEG45er2p5"),
-#     L1ObjectMapInputTag = cms.InputTag( "hltGtStage2ObjectMap" ),
-#     L1GlobalInputTag = cms.InputTag( "hltGtStage2Digis" ),
-#     L1MuonInputTag = cms.InputTag( 'hltGtStage2Digis','Muon' ),
-#     L1MuonShowerInputTag = cms.InputTag( 'hltGtStage2Digis','MuonShower' ),
-#     L1EGammaInputTag = cms.InputTag( 'hltGtStage2Digis','EGamma' ),
-#     L1JetInputTag = cms.InputTag( 'hltGtStage2Digis','Jet' ),
-#     L1TauInputTag = cms.InputTag( 'hltGtStage2Digis','Tau' ),
-#     L1EtSumInputTag = cms.InputTag( 'hltGtStage2Digis','EtSum' )
-# )
-
-# process.hltL1sSingleJet170IorSingleJet180IorSingleJet200 = cms.EDFilter( "HLTL1TSeed",
-#     saveTags = cms.bool( True ),
-#     L1SeedsLogicalExpression = cms.string( "L1_SingleJet180 OR L1_SingleJet200"),
-#     L1ObjectMapInputTag = cms.InputTag( "hltGtStage2ObjectMap" ),
-#     L1GlobalInputTag = cms.InputTag( "hltGtStage2Digis" ),
-#     L1MuonInputTag = cms.InputTag( 'hltGtStage2Digis','Muon' ),
-#     L1MuonShowerInputTag = cms.InputTag( 'hltGtStage2Digis','MuonShower' ),
-#     L1EGammaInputTag = cms.InputTag( 'hltGtStage2Digis','EGamma' ),
-#     L1JetInputTag = cms.InputTag( 'hltGtStage2Digis','Jet' ),
-#     L1TauInputTag = cms.InputTag( 'hltGtStage2Digis','Tau' ),
-#     L1EtSumInputTag = cms.InputTag( 'hltGtStage2Digis','EtSum' )
-# )
-
-# process.hltL1sSingleEGNonIsoOrWithJetAndTau = cms.EDFilter( "HLTL1TSeed",
-#     saveTags = cms.bool( True ),
-#     L1SeedsLogicalExpression = cms.string( "L1_SingleEG34er2p5 OR L1_SingleEG36er2p5 OR L1_SingleEG38er2p5 OR L1_SingleEG40er2p5 OR L1_SingleJet180 OR L1_SingleJet200 OR L1_SingleTau120er2p1 OR L1_SingleTau130er2p1 OR L1_SingleEG42er2p5 OR L1_SingleEG45er2p5 OR L1_SingleEG60"),
-#     L1ObjectMapInputTag = cms.InputTag( "hltGtStage2ObjectMap" ),
-#     L1GlobalInputTag = cms.InputTag( "hltGtStage2Digis" ),
-#     L1MuonInputTag = cms.InputTag( 'hltGtStage2Digis','Muon' ),
-#     L1MuonShowerInputTag = cms.InputTag( 'hltGtStage2Digis','MuonShower' ),
-#     L1EGammaInputTag = cms.InputTag( 'hltGtStage2Digis','EGamma' ),
-#     L1JetInputTag = cms.InputTag( 'hltGtStage2Digis','Jet' ),
-#     L1TauInputTag = cms.InputTag( 'hltGtStage2Digis','Tau' ),
-#     L1EtSumInputTag = cms.InputTag( 'hltGtStage2Digis','EtSum' )
-# )
-
-# process.hltL1sSingleAndDoubleEGNonIsoOrWithEG26WithJetAndTau = cms.EDFilter( "HLTL1TSeed",
-#     saveTags = cms.bool( True ),
-#     L1SeedsLogicalExpression = cms.string( "L1_SingleEG34er2p5 OR L1_SingleEG36er2p5 OR L1_SingleEG38er2p5 OR L1_SingleEG40er2p5 OR L1_SingleEG42er2p5 OR L1_SingleEG45er2p5 OR L1_SingleEG60 OR L1_DoubleEG_22_10_er2p5 OR L1_DoubleEG_25_12_er2p5 OR L1_DoubleEG_25_14_er2p5 OR L1_SingleJet180 OR L1_SingleJet200 OR L1_SingleTau120er2p1 OR L1_SingleTau130er2p1"),
-#     L1ObjectMapInputTag = cms.InputTag( "hltGtStage2ObjectMap" ),
-#     L1GlobalInputTag = cms.InputTag( "hltGtStage2Digis" ),
-#     L1MuonInputTag = cms.InputTag( 'hltGtStage2Digis','Muon' ),
-#     L1MuonShowerInputTag = cms.InputTag( 'hltGtStage2Digis','MuonShower' ),
-#     L1EGammaInputTag = cms.InputTag( 'hltGtStage2Digis','EGamma' ),
-#     L1JetInputTag = cms.InputTag( 'hltGtStage2Digis','Jet' ),
-#     L1TauInputTag = cms.InputTag( 'hltGtStage2Digis','Tau' ),
-#     L1EtSumInputTag = cms.InputTag( 'hltGtStage2Digis','EtSum' )
-# )
-
-# # process.hltL1sSingleEGorFilter = cms.EDFilter( "HLTEgammaL1TMatchFilterRegional",
-# #     saveTags = cms.bool( True ),
-# #     candIsolatedTag = cms.InputTag( "hltEgammaCandidates" ),
-# #     l1IsolatedTag = cms.InputTag( 'hltGtStage2Digis','EGamma' ),
-# #     candNonIsolatedTag = cms.InputTag( "" ),
-# #     l1NonIsolatedTag = cms.InputTag( 'hltGtStage2Digis','EGamma' ),
-# #     L1SeedFilterTag = cms.InputTag( "hltL1sSingleEGor" ),
-# #     l1CenJetsTag = cms.InputTag( 'hltGtStage2Digis','Jet' ),
-# #     l1TausTag = cms.InputTag( 'hltGtStage2Digis','Tau' ),
-# #     ncandcut = cms.int32( 1 ),
-# #     doIsolated = cms.bool( False ),
-# #     region_eta_size = cms.double( 0.522 ),
-# #     region_eta_size_ecap = cms.double( 1.0 ),
-# #     region_phi_size = cms.double( 1.044 ),
-# #     barrel_end = cms.double( 1.4791 ),
-# #     endcap_end = cms.double( 2.65 )
-# # )
-
+# Output file
+process.TFileService = cms.Service("TFileService",
+#    fileName = cms.string("Efficiency_Calculator_2024_Tracker_EOR3_Studies_Reference.root")
+    fileName = cms.string("Efficiency_Calculator_2024_Tracker_EOR3_Studies_Target_v1.root")
+#    fileName = cms.string("Efficiency_Calculator_2024_Tracker_EOR3_Studies_Target_v2.root")
+)
 
 process.EfficiencyCalculator = cms.EDAnalyzer('EfficiencyCalculator',
-                                              stageL1Trigger = cms.uint32(2)
+    stageL1Trigger = cms.uint32(2)
 )
-
-
 
 if reRunL1:
     process.myTask = cms.Task(process.GlobalParametersRcdSource,process.GlobalParameters,process.hltGtStage2Digis,process.hltGtStage2ObjectMap)
