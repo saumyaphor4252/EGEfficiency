5b1e110ab129028b4f0ea44520a1e146eb911cc7
Saumya Phor <saumya.phor4252@gmail.com>
2024-12-01 13:33:33 +0100

    Update the RAWRECO configs for 2024 Deepdive studies

--- TrigTools/plugins/EfficiencyCalculator.cc
+++ TrigTools/plugins/EfficiencyCalculator.cc
@@ -9,6 +9,7 @@
 #include "DataFormats/HLTReco/interface/TriggerEvent.h"
 #include "DataFormats/HLTReco/interface/TriggerObject.h"
 #include "DataFormats/HLTReco/interface/EgammaObject.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
 #include "CondFormats/DataRecord/interface/L1TGlobalParametersRcd.h"
 #include "DataFormats/HLTReco/interface/TriggerFilterObjectWithRefs.h"
 #include "DataFormats/HLTReco/interface/TriggerTypeDefs.h"
@@ -33,8 +34,9 @@ class EfficiencyCalculator : public edm::one::EDAnalyzer<edm::one::WatchRuns> {
  
 private:
   
-  std::string hltProcess_; //name of HLT process, usually "HLT"
-  edm::EDGetTokenT<std::vector<trigger::EgammaObject> > eleToken_;
+  std::string hltProcess_; // Name of HLT process, usually "HLT", but when customized: "HLTX" or "MYHLT"
+  // edm::EDGetTokenT<std::vector<trigger::EgammaObject> > eleToken_; // For RAW data
+  edm::EDGetTokenT<std::vector<reco::GsfElectron> > eleToken_; // For RAW-RECO data
   edm::EDGetTokenT<edm::TriggerResults > hltToken_;
   edm::EDGetTokenT<trigger::TriggerEvent > hltsevtToken_;
   edm::Service<TFileService> fs;
@@ -76,11 +78,11 @@ public:
 
 
 EfficiencyCalculator::EfficiencyCalculator(const edm::ParameterSet& iConfig):
-  hltProcess_("HLTX")
+  hltProcess_("MYHLT")
 {
-  eleToken_     = consumes<std::vector<trigger::EgammaObject> >(edm::InputTag("hltEgammaHLTExtra","","HLTX"));
-  hltToken_     = consumes<edm::TriggerResults>(edm::InputTag("TriggerResults","","HLTX"));
-  hltsevtToken_ = consumes<trigger::TriggerEvent>(edm::InputTag("hltTriggerSummaryAOD","","HLTX"));
+  eleToken_     = consumes<std::vector<reco::GsfElectron> >(edm::InputTag("gedGsfElectrons","","RECO")); // For RAW-RECO
+  hltToken_     = consumes<edm::TriggerResults>(edm::InputTag("TriggerResults","","MYHLT"));
+  hltsevtToken_ = consumes<trigger::TriggerEvent>(edm::InputTag("hltTriggerSummaryAOD","","MYHLT"));
   
   // Define all the histograms to be filled
   // pT
@@ -143,17 +145,7 @@ std::vector<const trigger::TriggerObject*> matchTrigObjs(const float eta,const f
   return matchedObjs;
 }
 
-const bool matchDRAndpT(const float eta1,const float phi1,const float pT1,const float eta2,const float phi2,const float pT2,const float maxDeltaR=0.1, const float maxDpT=0.1){
-  
-  bool isMatched = false;
-  const float maxDR2 = maxDeltaR*maxDeltaR;
-  const float deltaR2 = reco::deltaR2(eta1, phi1, eta2, phi2);
-  const float deltaPt = fabs(pT1-pT2)/pT1;
-  if(deltaR2 < maxDR2 && deltaPt < maxDpT) isMatched = true;
-  return isMatched;
-}
-
-float calculateInvMass(const trigger::EgammaObject tagElectron, const trigger::EgammaObject probeCandidate) {
+float calculateInvMass(const reco::GsfElectron tagElectron, const reco::GsfElectron probeCandidate) { // For RAW-RECO
 
   TLorentzVector tag;
   TLorentzVector probe;
@@ -174,8 +166,8 @@ void EfficiencyCalculator::beginRun(const edm::Run& run,const edm::EventSetup& s
 
 void EfficiencyCalculator::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-
-  edm::Handle<std::vector<trigger::EgammaObject> > ele;
+  // edm::Handle<std::vector<trigger::EgammaObject> > ele; // For RAW
+  edm::Handle<std::vector<reco::GsfElectron> > ele; // For RAW-RECO
   iEvent.getByToken(eleToken_,ele);
 
   edm::Handle<edm::TriggerResults > hlt;
@@ -184,94 +176,87 @@ void EfficiencyCalculator::analyze(const edm::Event& iEvent, const edm::EventSet
   edm::Handle<trigger::TriggerEvent > hltsevt;
   iEvent.getByToken(hltsevtToken_,hltsevt);
 
-  auto eg_trig_objs_filter = getListFilterPassedObj("hltEle32WPTightGsfTrackIsoFilter",*hltsevt.product());
+  auto trig_objs_filter = getListFilterPassedObj("hltEle32WPTightGsfTrackIsoFilter",*hltsevt.product());
 
   auto electrons = ele.product();
  
+  // Only retain events with at least two offline electrons
   if(electrons->size() < 2) return;
 
-  std::vector<trigger::EgammaObject> listOfTags;
+  //Create a list of tags
+  std::vector<reco::GsfElectron> listOfTags; //For RAW-RECO
 
   for(auto& el : *electrons){
-    if(fabs(el.eta()) > barrel_end_ || el.pt() < 32.)
-		continue;
+    if(fabs(el.eta()) > barrel_end_ || el.pt() < 32.) continue;
 
-    auto matchedTrigObjs = matchTrigObjs(el.eta(),el.phi(),el.pt(),eg_trig_objs_filter);
+    auto matchedTrigObjsTags = matchTrigObjs(el.eta(),el.phi(),el.pt(),trig_objs_filter);
 
-    if(matchedTrigObjs.size() > 0)
-		listOfTags.push_back(el);
+	for(auto trigObj : matchedTrigObjsTags){
+      listOfTags.push_back(el);
+	}
   }
 
+  // Now look for matching probes
+  bool isGoodPair = false;
   for(auto& el : *electrons){
 
     if(fabs(el.eta()) > endcap_end_) continue;
 
+	// Check the tag-probe invariant mass and charge compatibility with Z-->ee events
     for(auto tag : listOfTags){
-
       float invMass = calculateInvMass(tag, el);
-
-      if(fabs(invMass - 91.1876) < 30. && tag.id()*el.id() < 0)
-		break;
-      else
-		return;
+      isGoodPair = (fabs(invMass - 91.1876) < 30. && tag.pdgId()*el.pdgId() < 0)? true : false;
+	  if(isGoodPair) break;
     }
 
-    auto matched_objs_filter = matchTrigObjs(el.eta(),el.phi(),el.pt(),eg_trig_objs_filter);
-    auto nmatch_filter = matched_objs_filter.size();
-    
-    //Fill denominators
-    if (fabs(el.eta()) < 1.0 )
-		den_ele_pt_EB1->Fill(el.pt());
-    if (fabs(el.eta()) > 1.0 && fabs(el.eta()) < 1.44 )
-		den_ele_pt_EB2->Fill(el.pt());
-	if (fabs(el.eta()) < 1.44)
-		den_ele_pt_EB->Fill(el.pt());
+	// Only continue if a good probe is found
+    if(!isGoodPair) continue;
 
-	if (fabs(el.eta()) > 1.56 && fabs(el.eta()) < 2.0)
-		den_ele_pt_EE1->Fill(el.pt());
-    if (fabs(el.eta()) > 2.00 && fabs(el.eta()) < 2.5)
-		den_ele_pt_EE2->Fill(el.pt());
-    if (fabs(el.eta()) > 1.56 && fabs(el.eta()) < 2.5)
-		den_ele_pt_EE->Fill(el.pt());
+    // Fill denominators and occupancy histograms based on the probe passing the above ID
+    if (fabs(el.eta()) < 1.0 ) den_ele_pt_EB1->Fill(el.pt());
+    if (fabs(el.eta()) > 1.0 && fabs(el.eta()) < 1.44 ) den_ele_pt_EB2->Fill(el.pt());
+    if (fabs(el.eta()) < 1.44) den_ele_pt_EB->Fill(el.pt());
+	if (fabs(el.eta()) > 1.56 && fabs(el.eta()) < 2.0) den_ele_pt_EE1->Fill(el.pt());
+    if (fabs(el.eta()) > 2.00 && fabs(el.eta()) < 2.5) den_ele_pt_EE2->Fill(el.pt());
+    if (fabs(el.eta()) > 1.56 && fabs(el.eta()) < 2.5) den_ele_pt_EE->Fill(el.pt());
 
-    if ( (fabs(el.eta()) < 1.44) || (fabs(el.eta()) > 1.56 && fabs(el.eta()) < 2.5))
-		den_ele_pt->Fill(el.pt());
+    if ( (fabs(el.eta()) < 1.44) || (fabs(el.eta()) > 1.56 && fabs(el.eta()) < 2.5)) den_ele_pt->Fill(el.pt());
 
     if (el.pt() > 32.) {
-		den_ele_eta->Fill(el.eta());
-    	den_ele_phi->Fill(el.phi());
+      den_ele_eta->Fill(el.eta());
+      den_ele_phi->Fill(el.phi());
     }
-      
-    //Fill numerators
-    if(nmatch_filter>0){
 
-	  // Barrel
-      if (fabs(el.eta()) < 1.0 )
-		  num_ele_pt_EB1->Fill(el.pt());
-      if (fabs(el.eta()) > 1.0 && fabs(el.eta()) < 1.44)
-		  num_ele_pt_EB2->Fill(el.pt());
-	  if (fabs(el.eta()) < 1.44 )
-		  num_ele_pt_EB->Fill(el.pt());
+    // Create a list of probes matched to trigger objects based on DeltaR < 0.1
+	auto matchedTrigObjsProbes = matchTrigObjs(el.eta(),el.phi(),el.pt(),trig_objs_filter);
+    //auto matchedTrigObjsProbes = matchTrigObjs(el.eta(),el.phi(),unpackedTriggerObjects);
+    auto nmatch_filter = matchedTrigObjsProbes.size();
 
-	  // Endcap
-      if (fabs(el.eta()) > 1.56 && fabs(el.eta()) < 2.0)
-		  num_ele_pt_EE1->Fill(el.pt());
-      if (fabs(el.eta()) > 2.00 && fabs(el.eta()) < 2.5)
-		  num_ele_pt_EE2->Fill(el.pt());
-	  if (fabs(el.eta()) > 1.56 && fabs(el.eta()) < 2.5)
-		  num_ele_pt_EE->Fill(el.pt());
+    // Fill numerators based on the passing of a certain trigger filter
+	if(nmatch_filter>0){
+      for(auto trigObj : matchedTrigObjsProbes){
 
-      // Full
-	  if ( (fabs(el.eta()) < 1.44) || (fabs(el.eta()) > 1.56 && fabs(el.eta()) < 2.5))
-		  num_ele_pt->Fill(el.pt());
+	    // Barrel
+        if (fabs(el.eta()) < 1.0 ) num_ele_pt_EB1->Fill(el.pt());
+        if (fabs(el.eta()) > 1.0 && fabs(el.eta()) < 1.44) num_ele_pt_EB2->Fill(el.pt());
+        if (fabs(el.eta()) < 1.44 ) num_ele_pt_EB->Fill(el.pt());
 
-      if (el.pt() > 32.) {
-    	num_ele_eta->Fill(el.eta());
-    	num_ele_phi->Fill(el.phi());
+        // Endcap
+        if (fabs(el.eta()) > 1.56 && fabs(el.eta()) < 2.0) num_ele_pt_EE1->Fill(el.pt());
+        if (fabs(el.eta()) > 2.00 && fabs(el.eta()) < 2.5) num_ele_pt_EE2->Fill(el.pt());
+        if (fabs(el.eta()) > 1.56 && fabs(el.eta()) < 2.5) num_ele_pt_EE->Fill(el.pt());
 
-	    occupancy_phi_eta_all->Fill(el.eta(),el.phi());
-      }
+        // Full
+        if ( (fabs(el.eta()) < 1.44) || (fabs(el.eta()) > 1.56 && fabs(el.eta()) < 2.5)) num_ele_pt->Fill(el.pt());
 
+        if (el.pt() > 32.) {
+          num_ele_eta->Fill(el.eta());
+          num_ele_phi->Fill(el.phi());
+	      occupancy_phi_eta_all->Fill(el.eta(),el.phi());
+        }
+	    break; // Avoid to fill the numerator more than once with the same object if more than one offline-online matching is found
+        
+	  }	
     }
   }
 }
